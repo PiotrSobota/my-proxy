c87170f2d6c0cf124e15129a00301a4b642c9218
Piotr Sobota <119323404+PiotrSobotaUS@users.noreply.github.com>
2024-04-29 16:01:09 +0200

    V4 Use logging utils instead of logger.info

--- my-proxy-impl/src/main/java/com/x/eleven/services/ClientRequestCounterService.java
+++ my-proxy-impl/src/main/java/com/x/eleven/services/ClientRequestCounterService.java
@@ -1,17 +1,20 @@
 package com.x.eleven.services;
 
 import com.x.eleven.dbmock.DbServiceSimpleMock;
+import com.x.eleven.logger.LoggerUtils;
 import com.x.eleven.payload.requests.ClientRequest;
 import com.x.eleven.payload.requests.Request;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ClientRequestCounterService {
 
-    private final Logger logger = LoggerFactory.getLogger(ClientRequestCounterService.class);
+    private final LoggerUtils loggerUtils;
+
+    public ClientRequestCounterService(LoggerUtils loggerUtils) {
+        this.loggerUtils = loggerUtils;
+    }
 
     public Optional<Long> countClientRequests(DbServiceSimpleMock dbServiceMock, List<Request> requestsBelowIndex,
             int searchedIndex) {
@@ -23,7 +26,7 @@ public class ClientRequestCounterService {
                     .filter(ClientRequest.class::isInstance)
                     .count();
 
-            logger.info("{} : {}", "Object description", clientRequest);
+            loggerUtils.logClientRequestDescription(clientRequest);
             return Optional.of(processedClientRequestsCount);
         } else {
             return Optional.empty();
